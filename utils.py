062576824f9b6f239bda78e6c8115d9c8c4945aa
PasqualeEsposito <esposito.pascal@gmail.com>
2024-02-14 10:51:13 +0100

    Files updated

--- utils.py
+++ utils.py
@@ -1,169 +1,102 @@
+import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
 
-def draw_pitch(pitch, line, orientation, view):
-    """
-    Draw a soccer pitch given the pitch, the orientation, the view and the line
+def extract_teams(teams_df, type, area=""):
+    if type == 'club':
+        return teams_df[(teams_df['area'] == area) & (teams_df['type'] == 'club')]
+    elif type == 'national':
+        return teams_df[(teams_df['type'] == 'national')]
+
+
+def prepare_teams(teams_df):
+    teams_df['area'] = teams_df['area'].apply(lambda x: x.get('name'))
+    return teams_df
+
+
+def extract_events(events_df, area):
+    events = events_df[area][events_df[area]['playerId'] != 0]
+    return events
+
+
+def prepare_events(events):
+    events['x'] = events['positions'].apply(lambda x: x[0]['x'])
+    events['y'] = events['positions'].apply(lambda x: x[0]['y'])
+    events.drop(columns={'id', 'subEventName', 'positions', 'eventName'}, inplace=True)
+    return events
+
+
+def extract_role(role):
+    return role.get('code2')
+
+
+def prepare_players(players_df):
+    players_df['role'] = players_df['role'].apply(extract_role)
+    players_df.drop(columns={'passportArea','weight','height','foot','birthArea','birthDate','middleName','firstName','lastName', 'currentNationalTeamId', 'shortName', 'currentTeamId'},inplace=True)
+    players_df.rename(columns={'wyId':'playerId'}, inplace=True)
+    return players_df
+
+
+def compute_distance(df):
+    # Sort the dataframe by playerId, matchId, matchPeriod, gameweek, and eventSec
+    df.sort_values(by=['matchId', 'playerId', 'matchPeriod', 'eventSec'], inplace=True)
+
+    # Compute the distance between consecutive points
+    df['distance'] = np.sqrt(((df.groupby(['matchId', 'playerId', 'matchPeriod'])['x'].diff()**2) + (df.groupby(['playerId', 'matchId', 'matchPeriod'])['y'].diff()**2)))
+
+    # Fill NaN values with 0
+    df['distance'].fillna(0, inplace=True)
     
-    Parameters
-    ----------
-    pitch
-    
-    """
-    orientation = orientation
-    view = view
-    line = line
-    pitch = pitch
-    
-    if orientation.lower().startswith("h"):
-        
-        if view.lower().startswith("h"):
-            fig,ax = plt.subplots(figsize=(6.8,10.4))
-            plt.xlim(49,105)
-            plt.ylim(-1,69)
-        else:
-            fig,ax = plt.subplots(figsize=(10.4,6.8))
-            plt.xlim(-1,105)
-            plt.ylim(-1,69)
-        ax.axis('off') # this hides the x and y ticks
-    
-        # side and goal lines #
-        ly1 = [0,0,68,68,0]
-        lx1 = [0,104,104,0,0]
-
-        plt.plot(lx1,ly1,color=line,zorder=5)
+    return df
 
 
-        # boxes, 6 yard box and goals
-
-            #outer boxes#
-        ly2 = [13.84,13.84,54.16,54.16] 
-        lx2 = [104,87.5,87.5,104]
-        plt.plot(lx2,ly2,color=line,zorder=5)
-
-        ly3 = [13.84,13.84,54.16,54.16] 
-        lx3 = [0,16.5,16.5,0]
-        plt.plot(lx3,ly3,color=line,zorder=5)
-
-            #goals#
-        ly4 = [30.34,30.34,37.66,37.66]
-        lx4 = [104,104.2,104.2,104]
-        plt.plot(lx4,ly4,color=line,zorder=5)
-
-        ly5 = [30.34,30.34,37.66,37.66]
-        lx5 = [0,-0.2,-0.2,0]
-        plt.plot(lx5,ly5,color=line,zorder=5)
+def sum_distances(df):
+    tmp_df = df.groupby(['playerId', 'matchId'])['distance'].sum()
+    tmp_df = round(tmp_df, -2)
+    return pd.DataFrame(tmp_df)
 
 
-           #6 yard boxes#
-        ly6 = [24.84,24.84,43.16,43.16]
-        lx6 = [104,99.5,99.5,104]
-        plt.plot(lx6,ly6,color=line,zorder=5)
+def count_distances(df):
+    # Count the occurrences of each unique distance value
+    distance_counts = df['distance'].value_counts().reset_index()
 
-        ly7 = [24.84,24.84,43.16,43.16]
-        lx7 = [0,4.5,4.5,0]
-        plt.plot(lx7,ly7,color=line,zorder=5)
+    # Sort the DataFrame by distance
+    distance_counts = distance_counts.sort_values(by='distance')
 
-        #Halfway line, penalty spots, and kickoff spot
-        ly8 = [0,68] 
-        lx8 = [52,52]
-        plt.plot(lx8,ly8,color=line,zorder=5)
+    distance_counts['distance'] = distance_counts['distance'].astype(int)
+
+    return distance_counts
 
 
-        plt.scatter(93,34,color=line,zorder=5)
-        plt.scatter(11,34,color=line,zorder=5)
-        plt.scatter(52,34,color=line,zorder=5)
-
-        circle1 = plt.Circle((93.5,34), 9.15,ls='solid',lw=1.5,color=line, fill=False, zorder=1,alpha=1)
-        circle2 = plt.Circle((10.5,34), 9.15,ls='solid',lw=1.5,color=line, fill=False, zorder=1,alpha=1)
-        circle3 = plt.Circle((52, 34), 9.15,ls='solid',lw=1.5,color=line, fill=False, zorder=2,alpha=1)
-
-        ## Rectangles in boxes
-        rec1 = plt.Rectangle((87.5,20), 16,30,ls='-',color=pitch, zorder=1,alpha=1)
-        rec2 = plt.Rectangle((0, 20), 16.5,30,ls='-',color=pitch, zorder=1,alpha=1)
-
-        ## Pitch rectangle
-        rec3 = plt.Rectangle((-1, -1), 106,70,ls='-',color=pitch, zorder=1,alpha=1)
-
-        ax.add_artist(rec3)
-        ax.add_artist(circle1)
-        ax.add_artist(circle2)
-        ax.add_artist(rec1)
-        ax.add_artist(rec2)
-        ax.add_artist(circle3)
-        
-    else:
-        if view.lower().startswith("h"):
-            fig,ax = plt.subplots(figsize=(10.4,6.8))
-            plt.ylim(49,105)
-            plt.xlim(-1,69)
-        else:
-            fig,ax = plt.subplots(figsize=(6.8,10.4))
-            plt.ylim(-1,105)
-            plt.xlim(-1,69)
-        ax.axis('off') # this hides the x and y ticks
-
-        # side and goal lines #
-        lx1 = [0,0,68,68,0]
-        ly1 = [0,104,104,0,0]
-
-        plt.plot(lx1,ly1,color=line,zorder=5)
+def plot_histogram(df):
+    plt.figure(figsize=(15, 6))
+    sns.barplot(data=df, x='distance', y='count', color='skyblue')
+    plt.xlabel('Distance')
+    plt.ylabel('Count')
+    plt.title('Count of Distances')
+    plt.xticks(rotation=45)
+    plt.show()
 
 
-        # boxes, 6 yard box and goals
+def group_by_distance_role(df):
+    # Count the occurrences of each unique distance value
+    distance_counts_by_role = df[['distance', 'role']].value_counts().reset_index()
 
-            #outer boxes#
-        lx2 = [13.84,13.84,54.16,54.16] 
-        ly2 = [104,87.5,87.5,104]
-        plt.plot(lx2,ly2,color=line,zorder=5)
+    # Sort the DataFrame by distance
+    distance_counts_by_role = distance_counts_by_role.sort_values(by='distance')
 
-        lx3 = [13.84,13.84,54.16,54.16] 
-        ly3 = [0,16.5,16.5,0]
-        plt.plot(lx3,ly3,color=line,zorder=5)
+    distance_counts_by_role['distance'] = distance_counts_by_role['distance'].astype(int)
 
-            #goals#
-        lx4 = [30.34,30.34,37.66,37.66]
-        ly4 = [104,104.2,104.2,104]
-        plt.plot(lx4,ly4,color=line,zorder=5)
-
-        lx5 = [30.34,30.34,37.66,37.66]
-        ly5 = [0,-0.2,-0.2,0]
-        plt.plot(lx5,ly5,color=line,zorder=5)
+    return distance_counts_by_role
 
 
-           #6 yard boxes#
-        lx6 = [24.84,24.84,43.16,43.16]
-        ly6 = [104,99.5,99.5,104]
-        plt.plot(lx6,ly6,color=line,zorder=5)
-
-        lx7 = [24.84,24.84,43.16,43.16]
-        ly7 = [0,4.5,4.5,0]
-        plt.plot(lx7,ly7,color=line,zorder=5)
-
-        #Halfway line, penalty spots, and kickoff spot
-        lx8 = [0,68] 
-        ly8 = [52,52]
-        plt.plot(lx8,ly8,color=line,zorder=5)
-
-
-        plt.scatter(34,93,color=line,zorder=5)
-        plt.scatter(34,11,color=line,zorder=5)
-        plt.scatter(34,52,color=line,zorder=5)
-
-        circle1 = plt.Circle((34,93.5), 9.15,ls='solid',lw=1.5,color=line, fill=False, zorder=1,alpha=1)
-        circle2 = plt.Circle((34,10.5), 9.15,ls='solid',lw=1.5,color=line, fill=False, zorder=1,alpha=1)
-        circle3 = plt.Circle((34,52), 9.15,ls='solid',lw=1.5,color=line, fill=False, zorder=2,alpha=1)
-
-
-        ## Rectangles in boxes
-        rec1 = plt.Rectangle((20, 87.5), 30,16.5,ls='-',color=pitch, zorder=1,alpha=1)
-        rec2 = plt.Rectangle((20, 0), 30,16.5,ls='-',color=pitch, zorder=1,alpha=1)
-
-        ## Pitch rectangle
-        rec3 = plt.Rectangle((-1, -1), 70,106,ls='-',color=pitch, zorder=1,alpha=1)
-
-        ax.add_artist(rec3)
-        ax.add_artist(circle1)
-        ax.add_artist(circle2)
-        ax.add_artist(rec1)
-        ax.add_artist(rec2)
-        ax.add_artist(circle3)
+def plot_histogram_per_role(df):
+    plt.figure(figsize=(25, 6))
+    sns.barplot(data=df, x='distance', y='count', hue='role')
+    plt.xlabel('Distance')
+    plt.ylabel('Count')
+    plt.title('Histogram of Distances by Role')
+    plt.legend(title='Role')
+    plt.xticks(rotation=45)
+    plt.show()
