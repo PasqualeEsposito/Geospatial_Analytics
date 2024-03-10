1b76a859e869bdf7c6560835d270fe983b497a96
PasqualeEsposito <esposito.pascal@gmail.com>
2024-03-10 19:35:47 +0100

    utils.py updated

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+from turtle import color
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -293,7 +294,7 @@ def draw_pitch(pitch, line, probabilities, prev_tile):
         center_y = left_corner[i][1] + 68/3 / 8
 
         # Create the text element with desired opacity
-        text = plt.Text(center_x, center_y, str(round(probabilities[i]*100)) + '%', ha='center', va='center', color='green', alpha=1)
+        text = plt.Text(center_x, center_y, str(round(probabilities[i]*100)) + '%', ha='center', va='center', color='black', alpha=1)
 
         rec_prob = plt.Rectangle(left_corner[i], 104/3, 68/3 ,ls='-',color='green', zorder=2, alpha=probabilities[i])
 
@@ -447,3 +448,94 @@ def winning_probability(dictionary, short_passes_value):
 
     value_counts = winning_probability_plot(df, short_passes_value)
     return value_counts
+
+
+# Input:
+# Output: 
+#
+# The function draws the tessellation of the pitch.
+def draw_tessellation():
+    line = 'black'
+    pitch = 'green'
+    
+    fig,ax = plt.subplots(figsize=(10.4,6.8))
+    plt.xlim(-1,105)
+    plt.ylim(-1,69)
+    ax.axis('off') # this hides the x and y ticks
+
+    # plot the tessellations
+    left_corner = [(0, 136/3), (0, 68/3), (0,  0), (104/3, 136/3), (104/3, 68/3), (104/3, 0), (208/3, 136/3), (208/3, 68/3), (208/3, 0)]
+    for i in range(9):
+        center_x = left_corner[i][0] + 104/3 / 2
+        center_y = left_corner[i][1] + 68/3 / 2
+        text = plt.Text(center_x, center_y, i, ha='center', weight='bold', va='center', color='white', alpha=1, zorder=10)
+        ax.add_artist(text)
+
+    # side and goal lines #
+    ly1 = [0,0,68,68,0]
+    lx1 = [0,104,104,0,0]
+
+    plt.plot(lx1,ly1,color=line,zorder=5)
+
+    # boxes, 6 yard box and goals
+
+        #outer boxes#
+    ly2 = [13.84,13.84,54.16,54.16] 
+    lx2 = [104,87.5,87.5,104]
+    plt.plot(lx2,ly2,color=line,zorder=5)
+
+    ly3 = [13.84,13.84,54.16,54.16] 
+    lx3 = [0,16.5,16.5,0]
+    plt.plot(lx3,ly3,color=line,zorder=5)
+
+        #goals#
+    ly4 = [30.34,30.34,37.66,37.66]
+    lx4 = [104,104.2,104.2,104]
+    plt.plot(lx4,ly4,color=line,zorder=5)
+
+    ly5 = [30.34,30.34,37.66,37.66]
+    lx5 = [0,-0.2,-0.2,0]
+    plt.plot(lx5,ly5,color=line,zorder=5)
+
+
+        #6 yard boxes#
+    ly6 = [24.84,24.84,43.16,43.16]
+    lx6 = [104,99.5,99.5,104]
+    plt.plot(lx6,ly6,color=line,zorder=5)
+
+    ly7 = [24.84,24.84,43.16,43.16]
+    lx7 = [0,4.5,4.5,0]
+    plt.plot(lx7,ly7,color=line,zorder=5)
+
+    #Halfway line, penalty spots, and kickoff spot
+    ly8 = [0,68] 
+    lx8 = [52,52]
+    plt.plot(lx8,ly8,color=line,zorder=5)
+
+
+    plt.scatter(93,34,color=line,zorder=5)
+    plt.scatter(11,34,color=line,zorder=5)
+    plt.scatter(52,34,color=line,zorder=5)
+
+    circle1 = plt.Circle((93.5,34), 9.15,ls='solid',lw=1.5,color=line, fill=False, zorder=1,alpha=1)
+    circle2 = plt.Circle((10.5,34), 9.15,ls='solid',lw=1.5,color=line, fill=False, zorder=1,alpha=1)
+    circle3 = plt.Circle((52, 34), 9.15,ls='solid',lw=1.5,color=line, fill=False, zorder=2,alpha=1)
+
+    ## Rectangles in boxes
+    rec1 = plt.Rectangle((87.5,20), 16,30,ls='-',color=pitch, zorder=1,alpha=1)
+    rec2 = plt.Rectangle((0, 20), 16.5,30,ls='-',color=pitch, zorder=1,alpha=1)
+
+    ## Pitch rectangle
+    rec3 = plt.Rectangle((-1, -1), 106,70,ls='-',color=pitch, zorder=1,alpha=1)
+
+    plt.plot([104/3, 104/3], [0, 68], color=line, zorder=5)
+    plt.plot([(104/3)*2, (104/3)*2], [0, 68], color=line, zorder=5)
+    plt.plot([0, 104], [68/3, 68/3], color=line, zorder=5)
+    plt.plot([0, 104], [(68/3)*2, (68/3)*2], color=line, zorder=5)
+
+    ax.add_artist(rec3)
+    ax.add_artist(circle1)
+    ax.add_artist(circle2)
+    ax.add_artist(rec1)
+    ax.add_artist(rec2)
+    ax.add_artist(circle3)
